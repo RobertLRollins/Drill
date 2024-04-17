d205f9abc1d51bdbd6db6701f2c2c4ad7024f321
RobertLRollins <robertlrollins@protonmail.com>
2024-04-17 06:04:28 -0400

    Zenva - Construct a Crafting System In Godot 4

---
+++ deposits/other_deposit.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var item_name : String
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	var item = load("res://Items/Item Data/" + item_name + ".tres")
+	Global.on_give_player_item.emit(item, 3)
+	
+	
+	var explosion_scene = load("res://deposits/explosion_particles.tscn")
+	var new_node = explosion_scene.instantiate()
+	new_node.position = self.position
+	new_node.emitting = true
+	self.get_parent().add_child(new_node)
+	
+	queue_free()
